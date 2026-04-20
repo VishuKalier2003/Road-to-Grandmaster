558e8bd7341a4760c15dd32aae34e4a255b4e4a7
VishuKalier2003 <vishukalier123@gmail.com>
2026-04-20 13:59:07 +0530

    math-800

---
+++ codeforces/newbie/rating800/UnitArray.java
@@ -0,0 +1,66 @@
+package codeforces.newbie.rating800;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+// https://codeforces.com/problemset/problem/1834/A
+public class UnitArray {
+    private static final DataInputStream IN = new DataInputStream(new BufferedInputStream(System.in, 1 << 16));
+    private static final StringBuilder OUT = new StringBuilder();
+    private static final PrintWriter PW = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+    public static int nextInt() throws IOException {
+        int c, x = 0;
+        do {c = IN.read();} while(c < ' ');
+        boolean neg = c == '-';
+        if(neg) c = IN.read();
+        do {x = x * 10 + (c - '0');} while((c = IN.read()) >= '0');
+        return neg ? -x : x;
+    }
+
+    public static long nextLong() throws IOException {
+        int c;
+        long x = 0l;
+        do{c = IN.read();} while(c < ' ');
+        boolean neg = c == '-';
+        if(neg) c = IN.read();
+        do {
+            x = x * 10 + (c - '0');
+        } while ((c = IN.read()) >= '0');
+        return neg ? -x : x;
+    }
+
+    public static void print(Object o) {OUT.append(o);}
+    public static void println(Object o) {OUT.append(o).append('\n');}
+    public static void flush() {PW.print(OUT); PW.flush();}
+
+    public static void main(String args[]) throws IOException {
+        int t = nextInt();
+        while(t-->0) {
+            int n = nextInt();
+            int p = 0, m = 0, c = 0;
+            for(int i = 1; i <= n; i++) {
+                int x = nextInt();
+                if(x == 1)
+                    p++;
+                else
+                    m++;
+            }
+            int diff = m-p;
+            if(diff > 0) {
+                int k = (diff+1)/2;
+                c += k;
+                m -= k;
+                p += k;
+            }
+            if((m&1)==1)
+                c++;
+            println(c);
+        }
+        flush();
+    }
+}
---
+++ z-notes/arrays/parity/operations-parity.md
@@ -0,0 +1,19 @@
+## Operations Parity
+
+### Trigger
+Multiple type of operations (sum, product, etc.) and satisfying parity
+
+### Key Idea
+- Find the larger overlapping operation that satisfies parity
+- Greedy more from larger overlap to inner operation
+
+### One Liner
+- find operation that triggers other operations, begin from top greedily
+
+### Mistake
+- Couldn't infer the type of operation to perform first
+
+### Problems
+| S.No. | Problem | Note | Rating |
+|-|-|-|-|
+| 1. | [1834A](https://codeforces.com/problemset/problem/1834/A) | two operations, sum and product | 800 |
