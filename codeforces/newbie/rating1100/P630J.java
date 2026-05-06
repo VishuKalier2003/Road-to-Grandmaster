535036328af84189c2f097bd79a1160cf780c3a2
VishuKalier2003 <vishukalier123@gmail.com>
2026-05-06 19:27:31 +0530

    updates

---
+++ codeforces/newbie/rating1100/P630J.java
@@ -0,0 +1,71 @@
+package codeforces.newbie.rating1100;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+public class P630J {
+    private static final DataInputStream IN = new DataInputStream(new BufferedInputStream(System.in, 1 << 16));
+    private static final StringBuilder OUT = new StringBuilder();
+    private static final PrintWriter PW = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+    public static int nextInt() throws IOException {
+        int c, x = 0;
+        do {c = IN.read();} while(c < ' ');
+        boolean neg = c == '-';
+        if(neg) c = IN.read();
+        do {x = x * 10 + (c - '0');} while((c = IN.read()) >= '0');
+        return neg ? -x : x;
+    }
+
+    public static long nextLong() throws IOException {
+        int c;
+        long x = 0l; 
+        do{c = IN.read();} while(c < ' ');
+        boolean neg = c == '-';
+        if(neg) c = IN.read();
+        do {
+            x = x * 10 + (c - '0');
+        } while ((c = IN.read()) >= '0');
+        return neg ? -x : x;
+    }
+
+    public static String next() throws IOException {
+        int c;
+        do {c = IN.read();} while(c < ' ');
+        StringBuilder sb = new StringBuilder();
+        do {
+            sb.append((char)c);
+        } while((c = IN.read()) > ' ');
+        return sb.toString();
+    }
+
+    public static void print(Object o) {OUT.append(o);}
+    public static void println(Object o) {OUT.append(o).append('\n');}
+    public static void flush() {PW.print(OUT); PW.flush();}
+
+    public static void main(String args[]) throws IOException {
+        println(solve(nextLong()));
+        flush();
+    }
+
+    public static long solve(long n) {
+        long lcm = 1;
+        for(int i = 2; i <= 10; i++)
+            lcm = lcm(lcm, i);
+        return n / lcm;
+    }
+
+    public static long lcm(long a, long b) {
+        return a * b / gcd(a, b);
+    }
+
+    public static long gcd(long a, long b) {
+        if(b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+}
---
+++ z-cp-notes/multiples/ranged-divisors.md
@@ -0,0 +1,23 @@
+## Ranged Multiples
+
+### Trigger
+When
+- problem asks to check a divisibility of a number across all the numbers in a range
+
+### Mistakes
+- Using division normally
+
+### Idea
+- Find lcm of the entire range and then check the feasibility of solution
+- The solution will be feasible if number x is divisible by lcm
+
+### One Liner
+- Evaluate lcm and reach the feasible solution
+
+### Problem
+
+Related problems are given below-
+
+| S. No. | Problem Name | Problem | Rating |
+|-|-|-|-|
+| 1. | Divisibility | [630J](https://codeforces.com/problemset/problem/630/J) | 1100 |
